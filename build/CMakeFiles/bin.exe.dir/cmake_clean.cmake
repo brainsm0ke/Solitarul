ee4de81dcc693210af1227b8327ceb3b9bc34b21
AdiPi2003 <adipi5996@gmail.com>
2023-12-07 10:07:47 +0200

    Migrare la VS Code

---
+++ build/CMakeFiles/bin.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin.exe.dir/main.cpp.obj"
+  "CMakeFiles/bin.exe.dir/main.cpp.obj.d"
+  "bin.exe.exe"
+  "bin.exe.exe.manifest"
+  "bin.exe.pdb"
+  "libbin.exe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
