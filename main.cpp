0f2c45842fcb3a50a2b6da244b037ff6bc0564cc
AdiPi2003 <adipi5996@gmail.com>
2023-12-18 15:02:34 +0200

    Refactorare cod

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 
 #define MAX 25
+#define CUL 1
+#define CUL_SEL 4
+#define BKG 14
+
 using namespace std;
 
 enum tipLoc {
@@ -11,29 +15,21 @@ enum tipLoc {
     piesa
 };
 
-int stanga,sus,width,height,latura,numar = 7, cul=1, sel=4;
-tipLoc TablaDeJoc[MAX][MAX];
-bool gata;
-void desen()
-{
-    int i,j;
-    width = 400;
-    height = 400;
-    latura = width/numar;
-    sus = (getmaxy() - width)/2;
-    stanga=(getmaxx() - height)/2;
-    setbkcolor(15);
-    clearviewport();
-    setcolor(1);
-    for (i = 1; i <= numar; i++)
-        for (j = 1; j <= numar; j++)
-            rectangle(stanga + latura * (i - 1),sus + latura * (j - 1),stanga + latura * i,sus + latura * j);
-}
-void deseneazaPiesa(int linia, int coloana, int cul)
+struct TablaDeJoc {
+    tipLoc loc[MAX][MAX];
+    int n;
+    int width;
+    int height;
+};
+
+void deseneazaPiesa(TablaDeJoc TablaDeJoc, int linia, int coloana, int cul)
 {
     int x1, y1, x2, y2;
     int xmijloc, ymijloc;
-    setcolor(15); setfillstyle(SOLID_FILL,15);
+    int latura = TablaDeJoc.width/TablaDeJoc.n;
+    int sus = (getmaxy() - TablaDeJoc.width)/2;
+    int stanga=(getmaxx() - TablaDeJoc.height)/2;
+    setcolor(BKG); setfillstyle(SOLID_FILL,BKG);
     x1=stanga+latura*(coloana-1); y1=sus+latura*(linia-1);
     x2=x1+latura; y2=y1+latura;
     xmijloc=(x1+x2)/2; ymijloc=(y1+y2)/2;
@@ -42,9 +38,29 @@ void deseneazaPiesa(int linia, int coloana, int cul)
     fillellipse(xmijloc,ymijloc,18,18);
 }
 
-void mutarePiesa(int jucator)
+void desen(TablaDeJoc TablaDeJoc)
+{
+    int i,j;
+    int latura = TablaDeJoc.width/TablaDeJoc.n;
+    int sus = (getmaxy() - TablaDeJoc.width)/2;
+    int stanga=(getmaxx() - TablaDeJoc.height)/2;
+    setbkcolor(BKG);
+    clearviewport();
+    setcolor(1);
+    for (i = 1; i <= TablaDeJoc.n; i++)
+        for (j = 1; j <= TablaDeJoc.n; j++)
+            if (TablaDeJoc.loc[i][j] != tipLoc::invalid){
+                rectangle(stanga + latura * (i - 1),sus + latura * (j - 1),stanga + latura * i,sus + latura * j);
+                if(TablaDeJoc.loc[i][j] == tipLoc::piesa) deseneazaPiesa(TablaDeJoc, i, j, CUL);
+            }
+}
+
+void mutarePiesa(TablaDeJoc &TablaDeJoc, int  jucator)
 {
     int linia1,coloana1,linia2,coloana2,x,y;
+    int latura = TablaDeJoc.width/TablaDeJoc.n;
+    int sus = (getmaxy() - TablaDeJoc.width)/2;
+    int stanga=(getmaxx() - TablaDeJoc.height)/2;
     int click=false;
     do
         if(ismouseclick(WM_LBUTTONDOWN) && !click)
@@ -55,8 +71,8 @@ void mutarePiesa(int jucator)
             linia1=(y-sus)/latura+1; coloana1=(x-stanga)/latura+1;
         }
     while (!click);
-    if(TablaDeJoc[linia1][coloana1] == tipLoc::piesa){
-        deseneazaPiesa(linia1,coloana1,sel);
+    if(TablaDeJoc.loc[linia1][coloana1] == tipLoc::piesa){
+        deseneazaPiesa(TablaDeJoc,linia1,coloana1,CUL_SEL);
         click=false;
         do
             if(ismouseclick(WM_LBUTTONDOWN) && !click)
@@ -67,42 +83,42 @@ void mutarePiesa(int jucator)
                 linia2=(y-sus)/latura+1; coloana2=(x-stanga)/latura+1;
             }
         while (!click);
-        deseneazaPiesa(linia1,coloana1, cul);
-        if(TablaDeJoc[linia2][coloana2] == tipLoc::gol){
+        deseneazaPiesa(TablaDeJoc,linia1,coloana1, CUL);
+        if(TablaDeJoc.loc[linia2][coloana2] == tipLoc::gol){
             if(coloana1 == coloana2){
-                if (linia1+2 == linia2 && TablaDeJoc[linia1+1][coloana1] == tipLoc::piesa) {
-                    TablaDeJoc[linia1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1, coloana1, 15);
-                    TablaDeJoc[linia1+1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1+1,coloana1, 15);
-                    TablaDeJoc[linia2][coloana2] = tipLoc::piesa;
-                    deseneazaPiesa(linia2, coloana2, cul);
+                if (linia1+2 == linia2 && TablaDeJoc.loc[linia1+1][coloana1] == tipLoc::piesa) {
+                    TablaDeJoc.loc[linia1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc ,linia1, coloana1, BKG);
+                    TablaDeJoc.loc[linia1+1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc ,linia1+1,coloana1, BKG);
+                    TablaDeJoc.loc[linia2][coloana2] = tipLoc::piesa;
+                    deseneazaPiesa(TablaDeJoc,linia2, coloana2, CUL);
                 }
-                else if(linia1-2 == linia2 && TablaDeJoc[linia1-1][coloana1] == tipLoc::piesa){
-                    TablaDeJoc[linia1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1, coloana1, 15);
-                    TablaDeJoc[linia1-1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1-1,coloana1, 15);
-                    TablaDeJoc[linia2][coloana2] = tipLoc::piesa;
-                    deseneazaPiesa(linia2, coloana2, cul);
+                else if(linia1-2 == linia2 && TablaDeJoc.loc[linia1-1][coloana1] == tipLoc::piesa){
+                    TablaDeJoc.loc[linia1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1, coloana1, BKG);
+                    TablaDeJoc.loc[linia1-1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1-1,coloana1, BKG);
+                    TablaDeJoc.loc[linia2][coloana2] = tipLoc::piesa;
+                    deseneazaPiesa(TablaDeJoc,linia2, coloana2, CUL);
                 }
             }
             else if(linia1 == linia2){
-                if(coloana1+2 == coloana2 && TablaDeJoc[linia1][coloana1+1] == tipLoc::piesa){
-                    TablaDeJoc[linia1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1, coloana1, 15);
-                    TablaDeJoc[linia1][coloana1+1] = tipLoc::gol;
-                    deseneazaPiesa(linia1,coloana1+1, 15);
-                    TablaDeJoc[linia2][coloana2] = tipLoc::piesa;
-                    deseneazaPiesa(linia2, coloana2, cul);
+                if(coloana1+2 == coloana2 && TablaDeJoc.loc[linia1][coloana1+1] == tipLoc::piesa){
+                    TablaDeJoc.loc[linia1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1, coloana1, BKG);
+                    TablaDeJoc.loc[linia1][coloana1+1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1,coloana1+1, BKG);
+                    TablaDeJoc.loc[linia2][coloana2] = tipLoc::piesa;
+                    deseneazaPiesa(TablaDeJoc,linia2, coloana2, CUL);
                 }
-                else if(coloana1-2 == coloana2 && TablaDeJoc[linia1][coloana1-1] == tipLoc::piesa){
-                    TablaDeJoc[linia1][coloana1] = tipLoc::gol;
-                    deseneazaPiesa(linia1, coloana1, 15);
-                    TablaDeJoc[linia1][coloana1-1] = tipLoc::gol;
-                    deseneazaPiesa(linia1,coloana1-1, 15);
-                    TablaDeJoc[linia2][coloana2] = tipLoc::piesa;
-                    deseneazaPiesa(linia2, coloana2, cul);
+                else if(coloana1-2 == coloana2 && TablaDeJoc.loc[linia1][coloana1-1] == tipLoc::piesa){
+                    TablaDeJoc.loc[linia1][coloana1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1, coloana1, BKG);
+                    TablaDeJoc.loc[linia1][coloana1-1] = tipLoc::gol;
+                    deseneazaPiesa(TablaDeJoc,linia1,coloana1-1, BKG);
+                    TablaDeJoc.loc[linia2][coloana2] = tipLoc::piesa;
+                    deseneazaPiesa(TablaDeJoc,linia2, coloana2, CUL);
                 }
             }
         }
@@ -116,22 +132,24 @@ bool castigat(int jucator)
 
 int main()
 {
-    int i,j;
-   initwindow(800,600);
-   desen();
-    for (i=1; i<=numar; i++)
-    for (j=1; j<=numar; j++)
+    TablaDeJoc T;
+    T.n = 7;
+    T.width = 400;
+    T.height = 400;
+    initwindow(800,600);
+    for (int i=1; i<=T.n; i++)
+    for (int j=1; j<=T.n; j++)
     {
-        if(i != numar/2+1 || j != numar/2+1)
+        if(i != 4 || j != 4)
         {
-            TablaDeJoc[i][j]=tipLoc::piesa;
-            deseneazaPiesa(i,j, cul);
+            T.loc[i][j]=tipLoc::piesa;
         }
-        else TablaDeJoc[i][j]=tipLoc::gol;
+        else T.loc[i][j]=tipLoc::gol;
     }
+    desen(T);
     do
     {
-       mutarePiesa(1);
+       mutarePiesa(T, 1);
     } while (!castigat(1) && !castigat(2));
    getch();
    closegraph();
